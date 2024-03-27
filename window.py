cceda9a3c62b713d8b525aa73d7f63e7dfb23b78
zxcGhoulSSSrank <161574316+zxcGhoulSSSrank@users.noreply.github.com>
2024-03-27 08:16:25 +0300

    Create window.py

---
+++ window.py
@@ -0,0 +1,57 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.button1 = QtWidgets.QPushButton(self.centralwidget)
+        self.button1.setGeometry(QtCore.QRect(660, 120, 93, 28))
+        self.button1.setObjectName("button1")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(320, 10, 121, 81))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        self.label.setFont(font)
+        self.label.setIndent(1)
+        self.label.setObjectName("label")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 90, 251, 80))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label_2 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout.addWidget(self.label_2)
+        self.doubleSpinBox = QtWidgets.QDoubleSpinBox(self.horizontalLayoutWidget)
+        self.doubleSpinBox.setObjectName("doubleSpinBox")
+        self.horizontalLayout.addWidget(self.doubleSpinBox)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(660, 180, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(20, 230, 231, 28))
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.Set_Text_To_Label = QtWidgets.QAction(MainWindow)
+        self.Set_Text_To_Label.setObjectName("Set_Text_To_Label")
+
+        self.retranslateUi(MainWindow)
+        self.button1.clicked['bool'].connect(self.Set_Text_To_Label.trigger) # type: ignore
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button1.setText(_translate("MainWindow", "Включить"))
+        self.label.setText(_translate("MainWindow", "Станок"))
+        self.label_2.setText(_translate("MainWindow", "Скорость "))
+        self.pushButton.setText(_translate("MainWindow", "Выключить"))
+        self.pushButton_2.setText(_translate("MainWindow", "Перейти к координатной сетке"))
+        self.Set_Text_To_Label.setText(_translate("MainWindow", "Set Text To Label"))
